23ec5903b783935ff003959011114b958387d80d
LWilliamson1 <larry.williamson2@gmail.com>
2019-12-16 15:39:08 -0500

    Updates

---
+++ precache-manifest.a300395b7abf18ccbf7383529cff4c16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da03a9c5428d8140ebcff359a49c5be2",
+    "url": "/dnd-ability-calculator/index.html"
+  },
+  {
+    "revision": "ae33554e4cf24535e304",
+    "url": "/dnd-ability-calculator/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d2baca56d7bc8c0ad71d",
+    "url": "/dnd-ability-calculator/static/css/main.e8552557.chunk.css"
+  },
+  {
+    "revision": "ae33554e4cf24535e304",
+    "url": "/dnd-ability-calculator/static/js/2.bdf97a1f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/dnd-ability-calculator/static/js/2.bdf97a1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2baca56d7bc8c0ad71d",
+    "url": "/dnd-ability-calculator/static/js/main.76e533f5.chunk.js"
+  },
+  {
+    "revision": "3f97b6fc1f11f068b699",
+    "url": "/dnd-ability-calculator/static/js/runtime-main.2200362d.js"
+  }
+]);
